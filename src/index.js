d529114a187621aba3bfa3d1a1297e14695c3d7d
Laura Quiroga <l.quiroga@uniandes.edu.co>
2022-12-01 15:31:46 -0500

    Agregar instalaciones e imports iniciales

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import { IntlProvider } from 'react-intl';
+
+import messages_en from "./locales/en.json";
+import messages_es from "./locales/es.json";
+
+const messages = {
+    'en': messages_en,
+    'es': messages_es
+  };
+//obtener el lenguaje preferido del navegador
+const language = navigator.language.split(/[-_]/)[0];
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <IntlProvider locale={navigator.language} messages={messages[language]}>
     <App />
-  </React.StrictMode>
+  </IntlProvider>
 );
 
 // If you want your app to work offline and load faster, you can change
